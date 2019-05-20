bbc22fcd86c0a61671a8efccae4f9474507aa9fd
LiuCaiHe <at@lang.so>
2019-05-20 11:25:25 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div
+    id="main"
+    class="app-main"
+  >
+    <transition
+      name="fadesd"
+      mode="out-in"
+    >
+      <router-view />
+    </transition>
+  </div>
+</template>
+
+<style lang="stylus">
+@import "~@/stylus/main"
+
+#main {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 40px;
+}
+</style>
